2fa238a3896c849d870405cf5a911314b3a85bb3
Engel Ariel Vargas Sánchez <engesanzincodj@gmail.com>
2025-11-16 20:35:18 -0600

    ya repare la url

--- app/src/main/java/com/example/kotlinycompose/Constants.kt
+++ app/src/main/java/com/example/kotlinycompose/Constants.kt
@@ -2,7 +2,7 @@ package com.example.kotlinycompose
 
 object Constants {
 
-    const val BASE_URL="https://api.restful-api.dev/objects/"
-    const val OBJECTS_PATH="/objects"
+    const val BASE_URL="https://api.restful-api.dev/"
+    const val OBJECTS_PATH="objects"
 
 }
